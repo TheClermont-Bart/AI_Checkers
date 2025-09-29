40d2e48a7410b654e751425ce3ef9f6a868b8a1f
Olivier <olivierclermontf@gmail.com>
2025-09-29 09:31:15 -0400

    MAJ 29 Sept. Bloquer sur deuxieme coup apres avoir destroy piece adverse

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -2,15 +2,60 @@ using UnityEngine;
 
 public class AI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private bool _isQueen;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void moveAI(Tile tileSelected, Piece pieceSelected, Board _board,UI _ui)
+    {
+        if (pieceSelected == null || tileSelected == null) return;
+
+        Tile startTile = pieceSelected._tile;
+
+        if (pieceSelected.tag == "P2" && tileSelected.yPos == startTile.yPos + 2 && (tileSelected.xPos == startTile.xPos + 2 || tileSelected.xPos == startTile.xPos - 2))
+        {
+            int jumpedX = (startTile.xPos + tileSelected.xPos) / 2;
+            int jumpedY = (startTile.yPos + tileSelected.yPos) / 2;
+
+            Tile jumpedTile = _board._grid[jumpedY][jumpedX - 1];
+
+            if (jumpedTile != null && jumpedTile._piece != null && jumpedTile._piece.tag == "P1")
+            {
+
+                Destroy(jumpedTile._piece.gameObject);
+                jumpedTile._piece = null;
+                startTile._piece = null;
+
+                tileSelected._piece = pieceSelected;
+                pieceSelected._tile = tileSelected;
+
+                pieceSelected.transform.position = tileSelected.transform.position;
+
+                _board._currentPlayer = "P1";
+            }
+        }
+        else if (pieceSelected.tag == "P2" && tileSelected.yPos == startTile.yPos + 1 && (tileSelected.xPos == startTile.xPos + 1 || tileSelected.xPos == startTile.xPos - 1))
+        {
+            if (tileSelected._piece == null)
+            {
+                startTile._piece = null;
+                tileSelected._piece = pieceSelected;
+                pieceSelected._tile = tileSelected;
+                pieceSelected.transform.position = tileSelected.transform.position;
+
+                _board._currentPlayer = "P1";
+            }
+        }
+
+
+        _ui.PlayerChange(_board._currentPlayer);
     }
 }
--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -9,31 +9,36 @@ using UnityEngine.UIElements;
 public class Board : MonoBehaviour
 {
     [SerializeField] private UI _ui;
+    [SerializeField] private AI _ai;
     public Tile tileSelected;
     public Piece pieceSelected;
     public List<GameObject> gameObjects;
-    private string _currentPlayer = "P1";
+    public string _currentPlayer = "P1";
     [SerializeField] List<Tile> _tilelist;
-    private List<List<Tile>> _grid;
+    public List<List<Tile>> _grid;
 
     void Start()
     {
         _tilelist = FindObjectsByType<Tile>(FindObjectsSortMode.None).ToList();
 
         Tile _newTile = new Tile();
-        List<List<Tile>> _grid = new List<List<Tile>>();
+
+        _grid = new List<List<Tile>>();
+
         List<Tile> _gridLine = Enumerable.Repeat(_newTile, 5).ToList();
 
         for (int i = 0; i < 5; i++)
         {
-            _grid.Add(_gridLine);
+            _grid.Add(new List<Tile>(new Tile[5]));
         }
 
         foreach (Tile tile in _tilelist)
         {
-            _grid[tile.xPos - 1][tile.yPos] = tile;
+            if (tile.xPos > 0 && tile.xPos <= 5 && tile.yPos >= 0 && tile.yPos < 5)
+            {
+                _grid[tile.yPos][tile.xPos - 1] = tile;
+            }
         }
-
 
         _ui.PlayerChange(_currentPlayer);
     }
@@ -64,45 +69,63 @@ public class Board : MonoBehaviour
     public void movePiece()
     {
 
-        if (pieceSelected.tag == "P1")
+        if (pieceSelected == null || tileSelected == null) return;
+
+        Tile startTile = pieceSelected._tile;
+        
+        if (pieceSelected.tag == "P1" && checkDoubleJump(pieceSelected))
         {
-            if ((_grid[tileSelected.xPos + 1][tileSelected.yPos + 1].tag == "P2" || _grid[tileSelected.xPos - 1][tileSelected.yPos + 1].tag == "P2"))
+            int jumpedX = (startTile.xPos + tileSelected.xPos) / 2;
+            int jumpedY = (startTile.yPos + tileSelected.yPos) / 2;
+
+            Tile jumpedTile = _grid[jumpedY][jumpedX - 1];
+
+            if (jumpedTile != null && jumpedTile._piece != null && jumpedTile._piece.tag == "P2")
             {
-                if(tileSelected.yPos == pieceSelected._tile.yPos - 2 && (tileSelected.xPos == pieceSelected._tile.xPos - 2 || tileSelected.xPos == pieceSelected._tile.xPos + 2))
-                {
-                    pieceSelected.transform.position = tileSelected.transform.position;
-                    //_currentPlayer = _currentPlayer == "P1" ? "P2" : "P1";
-                }
+
+                Destroy(jumpedTile._piece.gameObject);
+                jumpedTile._piece = null;
+                startTile._piece = null;
+
+                tileSelected._piece = pieceSelected;
+                pieceSelected._tile = tileSelected;
+
+                pieceSelected.transform.position = tileSelected.transform.position;
             }
         }
-
-
-
-
-
-
-
-
-
-
-
-        /*if (pieceSelected.tag == "P1" && tileSelected.yPos == pieceSelected._tile.yPos - 1 && (tileSelected.xPos == pieceSelected._tile.xPos - 1 || tileSelected.xPos == pieceSelected._tile.xPos + 1))
-         {
-             pieceSelected.transform.position = tileSelected.transform.position;
-             pieceSelected._tile.isFreeSet = true;
-             tileSelected.isFreeSet = false;
-             _currentPlayer = _currentPlayer == "P1" ? "P2" : "P1";
-         }
-
-         if (pieceSelected.tag == "P2" && tileSelected.yPos == pieceSelected._tile.yPos + 1 && (tileSelected.xPos == pieceSelected._tile.xPos - 1 || tileSelected.xPos == pieceSelected._tile.xPos + 1))
-         {
-             pieceSelected.transform.position = tileSelected.transform.position;
-             pieceSelected._tile.isFreeSet = true;
-             tileSelected.isFreeSet = false;
-             _currentPlayer = _currentPlayer == "P1" ? "P2" : "P1";
-         }*/
-
-        pieceSelected = null;
+        else if (pieceSelected.tag == "P1" && tileSelected.yPos == startTile.yPos - 1 && (tileSelected.xPos == startTile.xPos - 1 || tileSelected.xPos == startTile.xPos + 1))
+        {
+            if (tileSelected._piece == null) 
+            {
+                startTile._piece = null;
+                tileSelected._piece = pieceSelected;
+                pieceSelected._tile = tileSelected;
+                pieceSelected.transform.position = tileSelected.transform.position;
+                _currentPlayer = "P2";
+            }
+        }
         _ui.PlayerChange(_currentPlayer);
+        _ai.moveAI(tileSelected, pieceSelected, this, _ui);
+    }
+
+    public bool checkDoubleJump(Piece pieceSelected)
+    {
+        if (pieceSelected == null || tileSelected == null) return false;
+
+        Tile startTile = pieceSelected._tile;
+
+        if (pieceSelected.tag == _currentPlayer && tileSelected.yPos == startTile.yPos - 2 && (tileSelected.xPos == startTile.xPos - 2 || tileSelected.xPos == startTile.xPos + 2))
+        {
+            int jumpedX = (startTile.xPos + tileSelected.xPos) / 2;
+            int jumpedY = (startTile.yPos + tileSelected.yPos) / 2;
+
+            Tile jumpedTile = _grid[jumpedY][jumpedX - 1];
+
+            if (jumpedTile != null && jumpedTile._piece != null && jumpedTile._piece.tag == "P2")
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
